7d6482e5ebafa5ee183d12a4dafb64644e9b6a7a
Menna T-Allah Magdy <mennamagdykh@gmail.com>
2023-01-28 08:57:55 +0200

    Create auth context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import "./index.css";
 
 import App from "./App";
+import { AuthContextProvider } from "./store/auth-context";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
