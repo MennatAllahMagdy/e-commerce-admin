788546bc87ffb880dd658112abd0b7169cb1826f
Menna T-Allah Magdy <mennamagdykh@gmail.com>
2023-01-28 08:51:23 +0200

    Add react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "primereact/resources/themes/lara-light-blue/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css";
+
+import React, { Fragment } from "react";
+
+function App() {
+  return <Fragment>Home</Fragment>;
+}
+
+export default App;
